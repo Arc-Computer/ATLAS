d5373737a01448fe5b44bd1331f68156b19d10da
Jarrod Barnes <jbarnes850@gmail.com>
2025-11-23 12:19:38 -0500

    Fix db_url override at both top level and trainer level
    db_url is defined in both run/teacher_gkd.yaml (top level) and
    trainer/gkd.yaml. Need to override both to avoid ATLAS_DB_URL error.

--- scripts/launch_gear2_multinode.sh
+++ scripts/launch_gear2_multinode.sh
@@ -34,6 +34,7 @@ accelerate launch \
   --config-name train \
   run@_global_=teacher_gkd \
   data@_global_=gsm8k_gkd \
+  db_url=null \
   trainer.db_url=null \
   +trainer.temperature=0.6 \
   +trainer.max_steps=2500 \
